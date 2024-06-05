4da02dcfd7f4bc95a8d30ee97ed3bde7a90e1b28
nebirous <nebirous@hotmail.com>
2024-06-05 04:42:06 +0200

    clerk automation

---
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import * as LR from "@uploadcare/blocks";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  onUpload?: any;
+};
+
+LR.registerBlocks(LR);
+
+const UploadCareButton = ({ onUpload }: Props) => {
+  const router = useRouter();
+  const ctxProviderRef = useRef<
+    typeof LR.UploadCtxProvider.prototype & LR.UploadCtxProvider
+  >(null);
+
+  useEffect(() => {
+    const handleUpload = async (e: any) => {
+      const file = await onUpload(e.detail.cdnUrl);
+      if (file) {
+        router.refresh();
+      }
+    };
+
+    ctxProviderRef.current?.addEventListener(
+      "file-upload-success",
+      handleUpload,
+    );
+  }, []);
+
+  return (
+    <div>
+      <lr-config
+        ctx-name="my-uploader"
+        pubkey="db544c1c4a472dea5185"
+      ></lr-config>
+
+      <lr-file-uploader-regular
+        ctx-name="my-uploader"
+        css-src={`https://cdn.jsdelivr.net/npm/@uploadcare/blocks@0.35.2/web/lr-file-uploader-regular.min.css`}
+      ></lr-file-uploader-regular>
+
+      <lr-upload-ctx-provider ref={ctxProviderRef} ctx-name="my-uploader" />
+    </div>
+  );
+};
+
+export default UploadCareButton;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
-import type {Metadata} from 'next';
-import {Open_Sans} from 'next/font/google';
-import './globals.css';
-import {ThemeProvider} from '@/providers/theme-provider';
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const font = Open_Sans({subsets: ['latin']});
+const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Synco',
-  description: 'Automate your work wit Synco',
+  title: "Synco",
+  description: "Automate your work wit Synco",
 };
 
 export default function RootLayout({
@@ -16,17 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
