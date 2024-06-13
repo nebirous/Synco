502fc665f8cd27fe9086badacc543d0be69d9773
nebirous <nebirous@hotmail.com>
2024-06-13 02:09:49 +0200

    editor canvas

--- src/app/(main)/(pages)/layout.tsx
+++ src/app/(main)/(pages)/layout.tsx
@@ -5,7 +5,7 @@ type Props = { children: React.ReactNode };
 const Layout = ({ children }: Props) => {
   return (
     <div className="h-screen overflow-scroll rounded-l-3xl border-l-[1px] border-t-[1px] border-muted-foreground/20 pb-20">
-      <div className="w-full">{children}</div>
+      {children}
     </div>
   );
 };
