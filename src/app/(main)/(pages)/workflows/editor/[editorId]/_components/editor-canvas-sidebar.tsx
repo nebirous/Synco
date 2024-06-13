010ae4fdee1af9969d542d9a9b15ca2b467a033d
nebirous <nebirous@hotmail.com>
2024-06-13 19:57:35 +0200

    editor sidebar

--- src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-sidebar.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-sidebar.tsx
@@ -1,7 +1,19 @@
 "use client";
-import { EditorNodeType } from "@/lib/types";
+import { EditorCanvasTypes, EditorNodeType } from "@/lib/types";
+import { useNodeConnections } from "@/providers/connections-provider";
 import { useEditor } from "@/providers/editor-provider";
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Separator } from "@/components/ui/separator";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import EditorCanvasIconHelper from "./editor-canvas-card-icon-helper";
+import { EditorCanvasDefaultCardTypes } from "@/lib/constants";
+import { onDragStart } from "@/lib/editor-utils";
 
 type Props = {
   nodes: EditorNodeType[];
@@ -9,7 +21,46 @@ type Props = {
 
 const EditorCanvasSidebar = ({ nodes }: Props) => {
   const { state } = useEditor();
-  return <div>EditorCanvasSidebar</div>;
+  const { nodeConnection } = useNodeConnections();
+  return (
+    <aside>
+      <Tabs
+        defaultValue="actions"
+        className="h-screen overflow-scroll p-4 pb-24"
+      >
+        <TabsList className="w-full">
+          <TabsTrigger value="actions">Actions</TabsTrigger>
+          <TabsTrigger value="settings">Settings</TabsTrigger>
+        </TabsList>
+        <TabsContent value="actions" className="flex flex-col gap-4 p-4">
+          {Object.entries(EditorCanvasDefaultCardTypes)
+            .filter(
+              ([_, cardType]) =>
+                (!nodes.length && cardType.type === "Trigger") ||
+                (nodes.length && cardType.type === "Action"),
+            )
+            .map(([cardKey, cardValue]) => (
+              <Card
+                key={cardKey}
+                draggable
+                className="w-full cursor-grab border-black bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-900"
+                onDragStart={(event) =>
+                  onDragStart(event, cardKey as EditorCanvasTypes)
+                }
+              >
+                <CardHeader className="flex flex-row items-center gap-4 p-4">
+                  <EditorCanvasIconHelper type={cardKey as EditorCanvasTypes} />
+                  <CardTitle className="text-md">
+                    {cardKey}
+                    <CardDescription>{cardValue.description}</CardDescription>
+                  </CardTitle>
+                </CardHeader>
+              </Card>
+            ))}
+        </TabsContent>
+      </Tabs>
+    </aside>
+  );
 };
 
 export default EditorCanvasSidebar;
