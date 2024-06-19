015a056d31b88897bc89d24e4c5d64591abe2b12
nebirous <nebirous@hotmail.com>
2024-06-19 20:24:37 +0200

    Google connection

--- src/app/(main)/(pages)/workflows/editor/[editorId]/_actions/workflow-connections.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_actions/workflow-connections.tsx
@@ -1,12 +1,12 @@
-'use server'
+"use server";
 
-import { db } from '@/lib/db'
+import { db } from "@/lib/db";
 
 export const onCreateNodesEdges = async (
   flowId: string,
   nodes: string,
   edges: string,
-  flowPath: string
+  flowPath: string,
 ) => {
   const flow = await db.workflows.update({
     where: {
@@ -17,13 +17,13 @@ export const onCreateNodesEdges = async (
       edges,
       flowPath: flowPath,
     },
-  })
+  });
 
-  if (flow) return { message: 'flow saved' }
-}
+  if (flow) return { message: "flow saved" };
+};
 
 export const onFlowPublish = async (workflowId: string, state: boolean) => {
-  console.log(state)
+  console.log(state);
   const published = await db.workflows.update({
     where: {
       id: workflowId,
@@ -31,8 +31,21 @@ export const onFlowPublish = async (workflowId: string, state: boolean) => {
     data: {
       publish: state,
     },
-  })
+  });
 
-  if (published.publish) return 'Workflow published'
-  return 'Workflow unpublished'
-}
+  if (published.publish) return "Workflow published";
+  return "Workflow unpublished";
+};
+
+export const onGetNodesEdges = async (flowId: string) => {
+  const nodesEdges = await db.workflows.findUnique({
+    where: {
+      id: flowId,
+    },
+    select: {
+      nodes: true,
+      edges: true,
+    },
+  });
+  if (nodesEdges?.nodes && nodesEdges?.edges) return nodesEdges;
+};
--- src/app/(main)/(pages)/workflows/editor/[editorId]/_components/content-based-on-title.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/content-based-on-title.tsx
@@ -1,14 +1,22 @@
 import { ConnectionProviderProps } from "@/providers/connections-provider";
 import { EditorState } from "@/providers/editor-provider";
-import React from "react";
+import React, { useEffect } from "react";
 import { nodeMapper } from "@/lib/types";
 import { AccordionContent } from "@/components/ui/accordion";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import GoogleFileDetails from "./google-file-detail";
 import { onContentChange } from "@/lib/editor-utils";
 import GoogleDriveFiles from "./google-drive-files";
 import ActionButton from "./action-button";
+import { toast } from "sonner";
+import axios from "axios";
 
 export interface Option {
   value: string;
@@ -43,6 +51,21 @@ const ContentBasedOnTitle = ({
   const { selectedNode } = newState.editor;
   const title = selectedNode.data.title;
 
+  useEffect(() => {
+    const reqGoogle = async () => {
+      const response: { data: { message: { files: any } } } =
+        await axios.get("/api/drive");
+      if (response) {
+        console.log(response.data.message.files[0]);
+        toast.message("Fetched File");
+        setFile(response.data.message.files[0]);
+      } else {
+        toast.error("Something went wrong");
+      }
+    };
+    reqGoogle();
+  }, []);
+
   //@ts-ignore
   const nodeConnectionType: any = nodeConnection[nodeMapper[title]];
   if (!nodeConnectionType) return <p>Not connected</p>;
@@ -64,49 +87,51 @@ const ContentBasedOnTitle = ({
 
   if (!isConnected) return <p>Not connected</p>;
 
-  return (<AccordionContent>
-    <Card>
-      {title === 'Discord' && (
-        <CardHeader>
-          <CardTitle>{nodeConnectionType.webhookName}</CardTitle>
-          <CardDescription>{nodeConnectionType.guildName}</CardDescription>
-        </CardHeader>
-      )}
-      <div className="flex flex-col gap-3 px-6 py-3 pb-20">
-        <p>{title === 'Notion' ? 'Values to be stored' : 'Message'}</p>
-
-        <Input
-          type="text"
-          value={nodeConnectionType.content}
-          onChange={(event) => onContentChange(nodeConnection, title, event)}
-        />
-
-        {JSON.stringify(file) !== '{}' && title !== 'Google Drive' && (
-          <Card className="w-full">
-            <CardContent className="px-2 py-3">
-              <div className="flex flex-col gap-4">
-                <CardDescription>Drive File</CardDescription>
-                <div className="flex flex-wrap gap-2">
-                  <GoogleFileDetails
-                    nodeConnection={nodeConnection}
-                    title={title}
-                    gFile={file}
-                  />
-                </div>
-              </div>
-            </CardContent>
-          </Card>
+  return (
+    <AccordionContent>
+      <Card>
+        {title === "Discord" && (
+          <CardHeader>
+            <CardTitle>{nodeConnectionType.webhookName}</CardTitle>
+            <CardDescription>{nodeConnectionType.guildName}</CardDescription>
+          </CardHeader>
         )}
-        {title === 'Google Drive' && <GoogleDriveFiles />}
-        <ActionButton
-          currentService={title}
-          nodeConnection={nodeConnection}
-          channels={selectedSlackChannels}
-          setChannels={setSelectedSlackChannels}
-        />
-      </div>
-    </Card>
-  </AccordionContent>)
+        <div className="flex flex-col gap-3 px-6 py-3 pb-20">
+          <p>{title === "Notion" ? "Values to be stored" : "Message"}</p>
+
+          <Input
+            type="text"
+            value={nodeConnectionType.content}
+            onChange={(event) => onContentChange(nodeConnection, title, event)}
+          />
+
+          {JSON.stringify(file) !== "{}" && title !== "Google Drive" && (
+            <Card className="w-full">
+              <CardContent className="px-2 py-3">
+                <div className="flex flex-col gap-4">
+                  <CardDescription>Drive File</CardDescription>
+                  <div className="flex flex-wrap gap-2">
+                    <GoogleFileDetails
+                      nodeConnection={nodeConnection}
+                      title={title}
+                      gFile={file}
+                    />
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          )}
+          {title === "Google Drive" && <GoogleDriveFiles />}
+          <ActionButton
+            currentService={title}
+            nodeConnection={nodeConnection}
+            channels={selectedSlackChannels}
+            setChannels={setSelectedSlackChannels}
+          />
+        </div>
+      </Card>
+    </AccordionContent>
+  );
 };
 
 export default ContentBasedOnTitle;
